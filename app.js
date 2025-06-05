9872d731b08ec34a1da6ddbc064534df9878ed59
Murilo Ferreira Santos <murilo.fsantos1@senacsp.edu.br>
2025-06-04 22:11:22 -0300

    Extensão MySQL2 e conexão com o Banco de Dados

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro) {
+    if (erro) {
+        console.error('Erro ao conectar ao banco de dados: ' + erro.stack);
+        return;
+    }
+    console.log('Conexão com banco de dados estabelecida com sucesso!');
+});
 
 app.get('/', function(req, res) {
-    res.write('Hello, World!');
+    res.write('Hello, Senac!');
     res.end();
     });
 
