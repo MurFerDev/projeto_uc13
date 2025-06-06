80ea4a8da63f588aab0d54bb7109ec46d8f122fd
Murilo Ferreira Santos <murilo.fsantos1@senacsp.edu.br>
2025-06-05 22:11:15 -0300

    Exercício: Criar uma nova rota

--- app.js
+++ app.js
@@ -26,8 +26,32 @@ conexao.connect(function(erro) {
     console.log('Conexão com banco de dados estabelecida com sucesso!');
 });
 
-app.get('/', function(req, res) {
-    res.render('teste');
+
+
+app.get('/clientes', (req, res) => {;
+    let sql = 'SELECT * FROM clientes';
+    conexao.query(sql, function(erro, clientes_qs) {
+        if (erro) {
+            console.error('Erro ao consultar clientes:', erro);
+            res.status(500).send('Erro ao consultar clientes');
+            return;
+        }
+        res.render('clientes', {clientes: clientes_qs});
     });
+}
+);
+
+app.get('/', (req, res) => {;
+    let sql = 'SELECT * FROM produtos';
+    conexao.query(sql, function(erro, produtos_qs) {
+        if (erro) {
+            console.error('Erro ao consultar produtos:', erro);
+            res.status(500).send('Erro ao consultar produtos');
+            return;
+        }
+        res.render('index', {produtos: produtos_qs});
+    });
+}
+);
 
 app.listen(8080);
